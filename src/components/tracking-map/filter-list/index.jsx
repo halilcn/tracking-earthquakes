04964cf264edee1419ba12aa3a098e740c871199
halilcn <iletisimhalilcan@gmail.com>
2023-03-31 18:55:48 +0300

    add selected status

--- src/components/tracking-map/filter-list/index.jsx
+++ src/components/tracking-map/filter-list/index.jsx
@@ -1,33 +1,69 @@
 import { BsCalendarDate } from 'react-icons/bs'
 import { BiFilterAlt } from 'react-icons/bi'
 import FilterArchive from './filter-archive'
+import FilterPanel from './filter-panel'
 import LoadingData from './loading-data'
+import { useCallback, useMemo, useState } from 'react'
 
 import './index.scss'
 
 const FilterList = () => {
+  const [filterContentType, setFilterContentType] = useState(null)
+
+  const FILTER_CONTENT_TYPE = {
+    ARCHIVE: 'archive',
+    FILTER_PANEL: 'filterPanel',
+  }
+
+  const MemoizedFilterContent = useCallback(() => {
+    if (filterContentType === FILTER_CONTENT_TYPE.ARCHIVE) return <FilterArchive />
+    if (filterContentType === FILTER_CONTENT_TYPE.FILTER_PANEL) return <FilterPanel />
+  }, [filterContentType])
+
+  const MemoizedFilterText = useCallback(() => {
+    if (filterContentType === FILTER_CONTENT_TYPE.ARCHIVE) return 'Geçmiş Depremler'
+    if (filterContentType === FILTER_CONTENT_TYPE.FILTER_PANEL) return 'Filtreler'
+  }, [filterContentType])
+
+  const isSelectedType = type => type === filterContentType
+
+  const handleSetFilterContentType = type => {
+    if (isSelectedType(type)) {
+      setFilterContentType(null)
+      return
+    }
+    setFilterContentType(type)
+  }
+
   return (
     <div className="filter">
       <div className="filter__list">
         <div className="filter__item">
           <LoadingData />
         </div>
-        <div className="filter__item filter__item--icon">
+        <div
+          onClick={() => handleSetFilterContentType(FILTER_CONTENT_TYPE.ARCHIVE)}
+          className={`filter__item filter__item--icon ${isSelectedType(FILTER_CONTENT_TYPE.ARCHIVE) && 'filter__item--selected'}`}>
           <BsCalendarDate className="filter__icon" />
           <div className="filter__icon-blur" />
         </div>
-        <div className="filter__item filter__item--icon">
+        <div
+          onClick={() => handleSetFilterContentType(FILTER_CONTENT_TYPE.FILTER_PANEL)}
+          className={`filter__item filter__item--icon ${isSelectedType(FILTER_CONTENT_TYPE.FILTER_PANEL) && 'filter__item--selected'}`}>
           <BiFilterAlt className="filter__icon" />
           <div className="filter__icon-blur" />
         </div>
       </div>
-      <div className="filter__content">
-        <div className="filter__title">Geçmiş Depremler</div>
-        <div className="filter__content-container">
-          <FilterArchive />
-          <div className="filter__icon-blur" />
+      {filterContentType && (
+        <div className="filter__content">
+          <div className="filter__title">
+            <MemoizedFilterText />
+          </div>
+          <div className="filter__content-container">
+            <MemoizedFilterContent />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
