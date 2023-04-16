2f9f2ff8fb2dcb9bc1194fb785e638311ae4bbd1
halilcn <iletisimhalilcan@gmail.com>
2023-04-16 16:53:02 +0300

    fix seaarch

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,7 +1,39 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { MAPBOX_API_KEY } from '../../constants'
 import './index.scss'
 
 const Search = () => {
-  return <div className="search">search only</div>
+  const mapCurrent = useSelector(state => state.earthquake.mapCurrent)
+
+  const setMapboxSearch = () => {
+    const searchInput = document.getElementById('search-input')
+    const geocoder = new MapboxGeocoder({
+      accessToken: MAPBOX_API_KEY,
+      placeholder: 'Bir yer ara...',
+      mapboxgl: mapboxgl,
+      marker: false,
+    })
+
+    if (searchInput.childNodes.length === 0 && mapCurrent) {
+      searchInput.appendChild(geocoder.onAdd(mapCurrent))
+    }
+  }
+
+  useEffect(() => {
+    setMapboxSearch()
+  }, [])
+
+  // TODO: we have to find better solution..
+  useEffect(() => {
+    setMapboxSearch()
+  }, [mapCurrent])
+
+  return (
+    <div className="search">
+      <div id="search-input" />
+    </div>
+  )
 }
 
 export default Search
