017bb101089b4d76cfd7a6d53460a26493e4425b
halilcn <iletisimhalilcan@gmail.com>
2023-06-14 15:39:03 +0300

    feat : add google tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import ReactGA from 'react-ga'
+
 import AppContainer from './components/app-container'
+import './i18n'
 import './styles/default.scss'
+
+ReactGA.initialize(process.env.VITE_TRACKING_ID)
 
 function App() {
   return <AppContainer />
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import './i18n'
 import storeConfig from './store/index.js'
 
 const store = storeConfig()
