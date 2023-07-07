96848327b275158cdfe38241bebc5230eb26c77a
halilcn <iletisimhalilcan@gmail.com>
2023-07-08 00:56:37 +0300

    ref : move handleGetArchiveEarthquakes to hook file

--- src/components/tracking-map/filter-list/filter-archive/index.jsx
+++ src/components/tracking-map/filter-list/filter-archive/index.jsx
@@ -3,16 +3,15 @@ import Button from '@mui/material/Button'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { ARCHIVE_CERTAIN_TIMES } from '../../../../constants'
 import constantsTestid from '../../../../constants/testid'
-import { handleEarthquakesKandilli, handleEarthquakesUsgs } from '../../../../service/earthquakes'
+import useEarthquakeArchive from '../../../../hooks/useEarthquakeArchive'
 import { earthquakeActions, isSelectedAnyArchiveItem } from '../../../../store/earthquake'
 import { convertDateFormatForAPI } from '../../../../utils'
-import { getPastEarthquakeDatesQueryParam } from '../../../../utils/queryParamsActions'
 import dayjs from './../../../../utils/dayjs'
 import './index.scss'
 
@@ -20,6 +19,7 @@ const FilterArchive = () => {
   const testid = constantsTestid.filterArchive
   const { t } = useTranslation()
   const dispatch = useDispatch()
+  const { handleGetArchiveEarthquakes } = useEarthquakeArchive()
 
   const archiveDate = useSelector(state => state.earthquake.archiveDate)
   const selectedFilterItem = useSelector(isSelectedAnyArchiveItem)
@@ -35,28 +35,6 @@ const FilterArchive = () => {
 
     dispatch(earthquakeActions.updateArchiveDate({ ...archiveDate, certainDate }))
     await handleGetArchiveEarthquakes(params)
-  }
-
-  const handleArchiveEarthquakesKandilli = async params => {
-    const preparedEarthquakesData = await handleEarthquakesKandilli(params)
-    dispatch(earthquakeActions.addEarthquakes(preparedEarthquakesData))
-  }
-
-  const handleArchiveEarthquakesUsgs = async params => {
-    const preparedEarthquakesData = await handleEarthquakesUsgs(params)
-    dispatch(earthquakeActions.addEarthquakes(preparedEarthquakesData))
-  }
-
-  const handleGetArchiveEarthquakes = async params => {
-    try {
-      dispatch(earthquakeActions.setIsLoadingData(true))
-      dispatch(earthquakeActions.setEarthquakes([]))
-      await Promise.all([handleArchiveEarthquakesKandilli(params), handleArchiveEarthquakesUsgs(params)])
-    } catch (err) {
-      alert(t('Occurred a problem'))
-    } finally {
-      dispatch(earthquakeActions.setIsLoadingData(false))
-    }
   }
 
   const handleChooseDate = async (date, type) => {
