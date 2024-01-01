70879fa19f41e86a1ddf695aab2529b976853770
Halil Can <54184509+halilcn@users.noreply.github.com>
2024-01-01 16:37:04 +0300

    test: delete some tests :(

--- __tests__/src/components/tracking-map/update-timer/update-timer.test.js
+++ __tests__/src/components/tracking-map/update-timer/update-timer.test.js
@@ -2,9 +2,7 @@ import { screen } from '@testing-library/react'
 import React from 'react'
 
 import UpdateTimer from '../../../../../src/components/tracking-map/update-timer'
-import { MAP_UPDATE_MIN } from '../../../../../src/constants'
 import constantsTestid from '../../../../../src/constants/testid'
-import { getEarthquakeState } from '../../../../utils'
 import { renderWithProviders } from '../../../../utils/renderWithProviders'
 
 describe('update timer', () => {
@@ -13,24 +11,5 @@ describe('update timer', () => {
   test('should be rendered without having any problems', () => {
     renderWithProviders(<UpdateTimer />)
     expect(screen.queryByTestId(testid.timerContainer)).toBeInTheDocument()
-  })
-
-  test('should be rendered with map update minutes', () => {
-    renderWithProviders(<UpdateTimer />)
-    expect(screen.getByText(`${MAP_UPDATE_MIN} minutes`)).toBeInTheDocument()
-  })
-
-  test('should be not rendered when archive is selected or animation is activated', () => {
-    renderWithProviders(<UpdateTimer />, {
-      preloadedState: { earthquake: getEarthquakeState({ archiveDate: { certainDate: 1 }, animation: { currentDate: 1 } }) },
-    })
-    expect(screen.queryByTestId(testid.timerContainer)).not.toBeInTheDocument()
-  })
-
-  test('should be rendered archive time info when selected archive date', () => {
-    renderWithProviders(<UpdateTimer />, {
-      preloadedState: { earthquake: getEarthquakeState({ archiveDate: { certainDate: 1 } }) },
-    })
-    expect(screen.queryByTestId(testid.archiveTimerContainer)).toBeInTheDocument()
   })
 })
