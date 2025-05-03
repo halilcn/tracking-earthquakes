07246afee585315aa3ad6bba1b8c0ba5d405d7df
Halil Can <54184509+halilcn@users.noreply.github.com>
2025-05-03 17:47:57 +0300

    feat: enhance EarthquakeItem component with improved styling and icon integration for depth and date

--- src/components/earthquake-list/earthquake-item/index.jsx
+++ src/components/earthquake-list/earthquake-item/index.jsx
@@ -1,9 +1,11 @@
 import { ListItem, ListItemButton } from '@mui/material'
 import React from 'react'
+import { FaClock } from 'react-icons/fa'
+import { IoLayers } from 'react-icons/io5'
 import { useSelector } from 'react-redux'
 
 import constantsTestId from '../../../constants/testid'
-import { isMobile } from '../../../utils'
+import { getPointColorByIntensity, isMobile } from '../../../utils'
 import dayjs from '../../../utils/dayjs'
 import './index.scss'
 
@@ -37,11 +39,19 @@ const EarthquakeItem = props => {
           </div>
         )}
         <div className="earthquake-raw__line">
-          <div className="earthquake-raw__item earthquake-raw__item--intensity">{earthquakeProperties.mag}</div>
-          <div className="earthquake-raw__circle-character">&#9679;</div>
-          <div className="earthquake-raw__item earthquake-raw__item--depth">{earthquakeProperties.depth} km</div>
-          <div className="earthquake-raw__circle-character">&#9679;</div>
-          <div className="earthquake-raw__item earthquake-raw__item--date">{dayjs(earthquakeProperties.date).format('DD MMM ddd - HH:mm')}</div>
+          <div
+            className="earthquake-raw__item earthquake-raw__item--intensity"
+            style={{ color: getPointColorByIntensity(earthquakeProperties.mag) }}>
+            {earthquakeProperties.mag}
+          </div>
+          <div className="earthquake-raw__item earthquake-raw__item--tag earthquake-raw__item--depth">
+            <IoLayers className="earthquake-raw__item-icon" />
+            <span>{earthquakeProperties.depth} km</span>
+          </div>
+          <div className="earthquake-raw__item earthquake-raw__item--tag earthquake-raw__item--date">
+            <FaClock className="earthquake-raw__item-icon" />
+            <span>{dayjs(earthquakeProperties.date).format('DD MMM ddd - HH:mm')}</span>
+          </div>
         </div>
         <div className="earthquake-raw__item earthquake-raw__item--title">{earthquakeProperties.title}</div>
       </ListItemButton>
